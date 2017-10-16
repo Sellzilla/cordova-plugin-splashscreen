5261c26b1dee8cf3d9ff9d6dc94646460314c46d
Doginal <alex.griggs@gmail.com>
2017-10-16 10:56:27 -0700

    added padding to android and ios

--- src/android/SplashScreen.java
+++ src/android/SplashScreen.java
@@ -365,6 +365,7 @@ public class SplashScreen extends CordovaPlugin {
                 ProgressBar progressBar = new ProgressBar(webView.getContext());
                 RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
                 layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+                layoutParams.addPadding(0, 0, 120, 0);
                 progressBar.setLayoutParams(layoutParams);
 
                 centeredLayout.addView(progressBar);
